4cf7b009c0a6cd84330cf07274678f5e99a87b05
RomanBeldent <romanbeldent@gmail.com>
2024-03-13 18:27:20 +0100

    api rest with db & sequelize ok

--- app.js
+++ app.js
@@ -16,4 +16,10 @@ app
 
 // Ici, nous placerons nos futurs points de terminaison.
 
+require('./src/routes/findAllPokemons')(app)
+require('./src/routes/findPokemonByPk')(app)
+require('./src/routes/createPokemon')(app)
+require('./src/routes/updatePokemon')(app)
+require('./src/routes/deletePokemon')(app)
+
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
