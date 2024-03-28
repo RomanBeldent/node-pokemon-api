40be50239ca466c45ab3010b2b472637fc94a7f6
RomanBeldent <romanbeldent@gmail.com>
2024-03-28 14:58:52 +0100

    limit wip

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -5,20 +5,21 @@ module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
     if (req.query.name) {
       const name = req.query.name
-      return Pokemon.findAndCountAll({ 
-        where: { 
+      return Pokemon.findAndCountAll({
+        where: {
           name: { // 'name' est la propriété du modèle pokémon
             [Op.like]: `%${name}%` // 'name' est le critère de la recherche
           }
         },
+        order: ['name'],
         limit: 5
       })
-        .then(({count, rows}) => {
+        .then(({ count, rows }) => {
           const message = `Il y a ${count} pokémons qui correspondent au terme de recherche ${name}.`
           res.json({ message, data: rows })
         })
     } else {
-      Pokemon.findAll()
+      Pokemon.findAll({ order: ['name'] })
         .then(pokemons => {
           const message = 'La liste des pokémons a bien été récupérée.'
           res.json({ message, data: pokemons })
