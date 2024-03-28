0d96793ec309896b902f315d571074beb48c29da
RomanBeldent <romanbeldent@gmail.com>
2024-03-28 14:47:24 +0100

    operator like research for name

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,23 +1,31 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require('sequelize')
 
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
     if (req.query.name) {
       const name = req.query.name
-      return Pokemon.findAll({ where: { name: name } })
+      return Pokemon.findAll({ 
+        where: { 
+          name: { // 'name' est la propriété du modèle pokémon
+            [Op.like]: `%${name}%` // 'name' est le critère de la recherche
+          }
+        } 
+      })
         .then(pokemons => {
           const message = `Il y a ${pokemons.length} pokémons qui correspondent au terme de recherche ${name}.`
           res.json({ message, data: pokemons })
         })
+    } else {
+      Pokemon.findAll()
+        .then(pokemons => {
+          const message = 'La liste des pokémons a bien été récupérée.'
+          res.json({ message, data: pokemons })
+        })
+        .catch(error => {
+          const message = 'La liste des pokémons n\'a pas pu être récupérée. Rééssayez dans quelques instants.'
+          res.status(500).json({ message, data: error })
+        })
     }
-    Pokemon.findAll()
-      .then(pokemons => {
-        const message = 'La liste des pokémons a bien été récupérée.'
-        res.json({ message, data: pokemons })
-      })
-      .catch(error => {
-        const message = 'La liste des pokémons n\'a pas pu être récupérée. Rééssayez dans quelques instants.'
-        res.status(500).json({ message, data: error })
-      })
   })
 }
